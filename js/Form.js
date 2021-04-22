7156e0bea073f0183100c3ecf718b59db57df0ed
Preeshika Dattani <73643686+preeshika-whitehat@users.noreply.github.com>
2021-04-22 18:37:00 +0800

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,33 @@
+class Form {
+    constructor() {
+
+    } 
+    display() {
+        var title = createElement("h2");
+        title.html("Car Racing Game");
+        title.position(130,10);
+
+        var input = createInput("name");
+        input.position(130,160);
+
+        var button = createButton("Start");
+        button.position(250,200);
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name = input.value();
+            playerCount = playerCount+1;
+
+            player.update(name);
+            player.updateCount(playerCount);
+
+            var greeting = createElement("h3");
+            greeting.html("Welcome to the Car Racing Game "+ name);
+            greeting.position(130,160);
+
+        })
+    }
+}
+
